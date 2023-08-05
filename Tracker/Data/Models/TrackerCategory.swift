5702c2be8891727182caf32c4ef47dffda87e348
Aleksandr Poliakov <master@artwist.ru>
2023-08-05 13:12:19 +0300

    Объявил трекер категорию

--- Tracker/Data/Models/TrackerCategory.swift
+++ Tracker/Data/Models/TrackerCategory.swift
@@ -6,6 +6,28 @@
 //
 
 import Foundation
-struct TrackerCategory {
-    var trackers: [Tracker]
+class TrackerCategory: TrackersStorageProtocol {
+    
+    var trackerTitle: String = ""
+    var trackers: [Tracker]?
+    
+    func addDay(to toPosition: Int, day date: SimpleDate, _ completion: () -> ()) {
+        if toPosition > trackers?.count ?? 0 {
+            print("Некорректный индекс")
+            return
+        } else {
+            trackers?[toPosition].isDoneAt.insert(date)
+        }
+    }
+    
+    func removeDay(to toPosition: Int, day date: SimpleDate, _ completion: () -> ()) {
+        if toPosition > trackers?.count {
+            print("Некорректный индекс")
+            return
+        } else {
+            trackers?[toPosition].isDoneAt.remove(date)
+        }
+    }
+    
+0
 }
