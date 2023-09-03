a870224effc5fcf78f68a8ca6b61cffd1cefb42c
Aleksandr Poliakov <master@artwist.ru>
2023-09-03 18:46:25 +0300

    Поменял индикацию пролистывания на индикацию перетаскивания

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -8,7 +8,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let onboardingController = OnboardingViewController()
+        let onboardingController = OnboardingViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal,
+            options: nil)
         
         if onboardingController.pages.isEmpty {
             window.rootViewController = TabBarController()
