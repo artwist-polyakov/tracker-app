90985f52edba301f42de3a127137dc10a3316c98
Aleksandr Poliakov <master@artwist.ru>
2023-09-07 14:53:09 +0300

    Добавил документацию к онбординг структуре данных

--- Tracker/Data/Models/OnboardingData.swift
+++ Tracker/Data/Models/OnboardingData.swift
@@ -2,6 +2,13 @@ import Foundation
 import UIKit
 
 struct OnboardingData {
+    
+    /*
+     title — заголовок, который будет выводиться в центре экрана
+     buttonText — текст на кнопке
+     imageName — картинки бывают двух видов OnboardingBackgroundRed или OnboardingBackgroundBlue
+     */
+    
     let title: String
     let buttonText: String
     let imageName: String
